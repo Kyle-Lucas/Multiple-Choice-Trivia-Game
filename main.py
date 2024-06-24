adeada2619e4971663c501c539618366f9814f1c
Kyle Lucas <kylo.lucas7@gmail.com>
2024-06-24 21:20:08 +0200

    More Setup and Test Questions

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ from kivy.core.window import Window
 Window.size = (480,860)
 
 class FSTG(App):
-    
-    Questions = ["In which game do you use the 'Coiled Sword Fragment'\n to respawn at last check point",
-                 "Who is Solaire of Astora?"]
 
-    Answers = ['Demons Souls', 'Dark Souls 2: SotFS', 'Dark Souls 3', 'Elden Ring']
+    Round = 0
+    Score = 0
+    Timer = 0
+    
+    Questions = ["In which game do you use the 'Coiled Sword Fragment'\n to respawn at the last Check Point",
+                 "What is required before you are able to Fast Travel \n in Dark souls (1)?",
+                 "What is the Name of the Onion Knight\n in Dark Souls 3?"]
+
+    Answers0 = ['Demons Souls', 'Dark Souls 2: SotFS', 'Dark Souls 3', 'Elden Ring']
+    Answers1 = ['Restore Humanity', 'Defeat 4 Great Souls', 'Defeat Ornstein and Smough', 'Place the Lord Vessel' ]
+    Answers2 = ['Siegward of Catarina', 'Siegmeyer of Catarina', 'Siegmayer of Catarina', 'Sieglinde of Catarina']
+
+    def LoadAns(self,widget):
+        pass
+
+    def CheckAns(self, widget):
+        if widget.text == 'Dark Souls 3':
+            print(f'Correct Answer!')
 
 if __name__ == '__main__':
     FSTG().run()
