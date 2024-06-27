8ff4f9e762d260a870520d74249d32eb11650c26
Kyle Lucas <kylo.lucas7@gmail.com>
2024-06-27 21:14:04 +0200

    Data.txt will store all Question and Answers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.core.window import Window
 
+import random
+
 Window.size = (480,860)
 
 class FSTG(App):
@@ -9,16 +11,12 @@ class FSTG(App):
     Score = 0
     Timer = 0
     
-    Questions = ["In which game do you use the 'Coiled Sword Fragment'\n to respawn at the last Check Point",
-                 "What is required before you are able to Fast Travel \n in Dark souls (1)?",
-                 "What is the Name of the Onion Knight\n in Dark Souls 3?"]
+    Question = None
+    Answers = []
 
-    Answers0 = ['Demons Souls', 'Dark Souls 2: SotFS', 'Dark Souls 3', 'Elden Ring']
-    Answers1 = ['Restore Humanity', 'Defeat 4 Great Souls', 'Defeat Ornstein and Smough', 'Place the Lord Vessel' ]
-    Answers2 = ['Siegward of Catarina', 'Siegmeyer of Catarina', 'Siegmayer of Catarina', 'Sieglinde of Catarina']
-
-    def LoadAns(self,widget):
-        pass
+    def LoadQue(self):
+        RandQue: str = random.choice(self.Questions)
+        self.root.ids.que.text = RandQue
 
     def CheckAns(self, widget):
         if widget.text == 'Dark Souls 3':
